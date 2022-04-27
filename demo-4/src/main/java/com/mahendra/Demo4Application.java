279c1c50a9cb32fb29f55240bfdb8ec6ddb78082
Mahendra Shinde <MahendraShinde@synergetics-india.com>
2022-04-27 12:00:38 +0530

    Rest Client

---
+++ demo-4/src/main/java/com/mahendra/Demo4Application.java
@@ -0,0 +1,40 @@
+package com.mahendra;
+
+import java.util.Collections;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+public class Demo4Application implements CommandLineRunner {
+
+	private RestTemplate template = new RestTemplate();
+	// URL for REST Service defined in demo-3 
+	private  final static String BASE_URL="http://localhost:8080/api/customer";
+	
+	public static void main(String[] args) {
+		SpringApplication.run(Demo4Application.class, args);
+	}
+	
+	@Override
+	public void run(String... args) throws Exception {
+		
+		System.out.println("Requesting HTML Output !");
+		HttpHeaders headers = new HttpHeaders();
+		headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
+		HttpEntity<String> entity = new HttpEntity<>("body",headers);
+		ResponseEntity<String> output = template.exchange(BASE_URL,HttpMethod.GET, entity, String.class);
+		System.out.println("Response Status Code : "+ output.getStatusCodeValue());
+		System.out.println("Response Text : "+ output.getBody());
+		
+	}
+
+}
